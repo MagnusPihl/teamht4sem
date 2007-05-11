01ccdc9694065f7132d8866c9293fbc141f1f98e
lmklmx <lmklmx@0d024abc-6e28-0410-acd8-a90324f98aa6>
2007-05-11 09:59:03 +0000

    New string rects

--- trunk/TVP4/src/game/visual/BitmapFont.java
+++ trunk/TVP4/src/game/visual/BitmapFont.java
@@ -6,11 +6,15 @@
  * Company: HT++
  *
  * @author LMK
- * @version 1.3
+ * @version 1.4
  *
- * Ideas: Add support for direction
+ * Ideas: 
+ * Add support for direction
+ * Only break on space or hyphen (-).
  *
  * ******VERSION HISTORY******
+ * LMK @ 11. maj 2007 (v 1.4)
+ * Added methods for drawing string rects.
  * LMK @ 23. april 2007 (v 1.3)
  * Fixed 0 width and height errors when using renderString.
  * LMK @ 17. april 2007 (v 1.2)
@@ -44,6 +48,8 @@ public class BitmapFont {
     private int[] charHeights;
     private int fontHeight;
     private int lineSpace;
+    private int dotIndex;
+    private int dotWidth;
         
     /**
      * Creates a new instance of BitmapFont using the files located in the
@@ -75,6 +81,13 @@ public class BitmapFont {
                     //throw new FileNotFoundException("Could not find character " + this.characters.charAt(i) + " in file " + currentFile.getPath());
                 }
             }
+            
+            this.dotIndex = CHARACTERS.indexOf(".");
+            if (this.dotIndex != -1) {
+                this.dotWidth = this.charWidths[this.dotIndex] * 3;
+            } else {
+                this.dotWidth = 0;
+            }
         } else {
             //throw new FileNotFoundException("The directory from which to load font doesn't exist: " + directory.getPath());
         }
@@ -90,7 +103,6 @@ public class BitmapFont {
      * @param x start position. To the left of text.
      * @param y start position. Above text.
      * @param width allowed to draw on. If 0 the string will be drawn vertically.
-     * @param distance between lines.
      * @return height in pixels used to draw the text
      */
     public int drawString(Graphics g, String string, int[] indexes, int startX, int startY, int maxLineWidth) {
@@ -124,20 +136,106 @@ public class BitmapFont {
     }
     
     /**
+     * Draw string on graphic width defined width and height.
+     * 
+     * @param graphic to draw on
+     * @param string to draw
+     * @param x start position. To the left of text.
+     * @param y start position. Above text.
+     * @param width allowed to draw on. If 0 the string will be drawn vertically.
+     * @param height allowed to draw on. If 0 a single line will be written.
+     * @return height in pixels used to draw the text
+     */
+    public int drawStringRect(Graphics g, String string, int[] indexes, int startX, int startY, int maxLineWidth, int maxHeight) {
+        int lineWidth = 0;
+        int textHeight = this.fontHeight;
+        int currentX = startX;
+        int currentY = startY + this.fontHeight;
+        maxHeight -= (this.fontHeight + this.lineSpace);
+        
+        for (int i = 0; i < string.length(); i++) {
+            if (indexes[i] != -1) {            
+                if (this.images[indexes[i]] != null) {
+                        
+                    if ((lineWidth != 0) && ((lineWidth + this.charWidths[indexes[i]]) > maxLineWidth)) {
+                        currentY += this.fontHeight + this.lineSpace;                        
+                        currentX = startX;
+                        lineWidth = 0;
+                        textHeight += this.fontHeight + this.lineSpace;
+                    } 
+                                        
+                    if (textHeight > maxHeight) {
+                        if (lineWidth + this.charWidths[indexes[i]] >= maxLineWidth - this.dotWidth) {   
+                            for (int j = 0; j < 3; j++) {
+                                g.drawImage(this.images[this.dotIndex], currentX, currentY - this.charHeights[this.dotIndex], null);                                    
+                                currentX += this.charWidths[dotIndex];
+                            }
+                            lineWidth += this.dotWidth;
+                            break;
+                        }                        
+                    }
+                    
+                    g.drawImage(this.images[indexes[i]], currentX, currentY - this.charHeights[indexes[i]], null);    
+                    
+                    lineWidth += this.charWidths[indexes[i]];                        
+                    currentX += this.charWidths[indexes[i]];
+                }
+                
+            } else if (string.charAt(i) == '\n') {
+                if (textHeight > maxHeight) {                    
+                    for (int j = 0; j < 3; j++) {
+                        g.drawImage(this.images[this.dotIndex], currentX, currentY - this.charHeights[this.dotIndex], null);                                    
+                        currentX += this.charWidths[dotIndex];
+                    }
+                    lineWidth += this.dotWidth;
+                    break;                
+                }
+                
+                currentY += this.fontHeight + lineSpace;
+                currentX = startX;
+                textHeight += this.fontHeight + this.lineSpace;
+                lineWidth = 0;
+            }
+        }
+        
+        return currentY - startY;
+    }
+    
+    /**
      * Prerender string so that i may be reused. An image of the appropriate
      * size created during prerender.
      *
      * @param String to render.
      * @param Maximum width.
      */
-    public BufferedImage renderString(String string, int maxLineWidth) {
-        //string.substring(0,30);
+    public BufferedImage renderString(String string, int maxLineWidth) {        
         int[] indexes = this.getIndexes(string);
         Dimension size = this.getStringBounds(string, indexes, maxLineWidth);
         
         if ((size.width != 0)&&(size.height != 0)) {
-            BufferedImage image = PacmanApp.getInstance().getCore().getScreenManager().createCompatibleImage(size.width, size.height, Transparency.TRANSLUCENT);        
+            BufferedImage image = PacmanApp.getInstance().getCore().getScreenManager().createCompatibleImage(size.width, size.height, Transparency.TRANSLUCENT);                    
             this.drawString(image.getGraphics(), string, indexes, 0, 0, maxLineWidth);
+            return image;
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Prerender string so that i may be reused. An image of the appropriate
+     * size created during prerender.
+     *
+     * @param String to render.
+     * @param Maximum width.
+     * @param Maximum height.
+     */
+    public BufferedImage renderStringRect(String string, int maxLineWidth, int maxHeight) {
+        int[] indexes = this.getIndexes(string);
+        Dimension size = this.getStringRectBounds(string, indexes, maxLineWidth, maxHeight);
+        
+        if ((size.width != 0)&&(size.height != 0)) {
+            BufferedImage image = PacmanApp.getInstance().getCore().getScreenManager().createCompatibleImage(size.width, size.height, Transparency.TRANSLUCENT);        
+            this.drawStringRect(image.getGraphics(), string, indexes, 0, 0, maxLineWidth,maxHeight);
             return image;
         }
         
@@ -154,8 +252,23 @@ public class BitmapFont {
      * @param width allowed to draw on. If 0 the string will be drawn vertically.
      * @return height in pixels used to draw the text
      */
-    public int drawString(Graphics g, String string, int startX, int startY, int maxLineWidth) {
+    public int drawString(Graphics g, String string, int startX, int startY, int maxLineWidth) {        
+        //return drawStringRect(g, string, this.getIndexes(string), startX, startY, 100, 0);
         return drawString(g, string, this.getIndexes(string), startX, startY, maxLineWidth);
+    }
+    
+    /**
+     * Draw string on graphic width defined width.
+     * 
+     * @param graphic to draw on
+     * @param string to draw
+     * @param x start position. To the left of text.
+     * @param y start position. Above text.
+     * @param width allowed to draw on. If 0 the string will be drawn vertically.
+     * @return height in pixels used to draw the text
+     */
+    public int drawStringRect(Graphics g, String string, int startX, int startY, int maxLineWidth) {        
+        return drawStringRect(g, string, this.getIndexes(string), startX, startY, maxLineWidth, 0);
     }
     
     /**
@@ -181,6 +294,14 @@ public class BitmapFont {
         return indexes;
     }
     
+    /**
+     * Get the pixel dimensions needed to draw the supplied string.
+     *
+     * @param string to calculate dimensions of.
+     * @param string indexes.
+     * @param width allowed to draw on. If 0 the string will be drawn vertically.
+     * @return Dimension.
+     */
     private Dimension getStringBounds(String string, int[] indexes, int maxLineWidth) {        
         int currentWidth = 0;
         int maxX = 0;
@@ -213,6 +334,62 @@ public class BitmapFont {
             maxX = currentWidth;
         }
         currentWidth = 0;
+        
+        return new Dimension(maxX, maxY);
+    }
+        
+    /**
+     * Get the pixel dimensions needed to draw the supplied string.
+     *
+     * @param string to calculate dimensions of.
+     * @param string indexes.
+     * @param width allowed to draw on. If 0 the string will be drawn vertically.
+     * @param height allowed to draw on. If 0 the string will be singleline.
+     * @return Dimension.
+     */
+    private Dimension getStringRectBounds(String string, int[] indexes, int maxLineWidth, int maxHeight) {        
+        int currentWidth = 0;        
+        int maxX = 0;
+        int maxY = this.fontHeight;        
+        maxHeight -= (this.fontHeight + this.lineSpace);
+        
+        for (int i = 0; i < string.length(); i++) {                        
+            if (indexes[i] != -1) {            
+                if (this.images[indexes[i]] != null) {
+                    
+                    if ((currentWidth != 0) && ((currentWidth + this.charWidths[indexes[i]]) > maxLineWidth)) {                        
+                        maxY += this.fontHeight + this.lineSpace;
+                        if (currentWidth > maxX) {
+                            maxX = currentWidth;
+                        }
+                        currentWidth = 0;                        
+                    } 
+                    
+                    if (maxY > maxHeight) {
+                        if (currentWidth + this.charWidths[indexes[i]] >= maxLineWidth - this.dotWidth) {                               
+                            currentWidth += this.dotWidth;
+                            break;
+                        }                        
+                    }
+                    
+                    currentWidth += this.charWidths[indexes[i]];
+                }
+            } else if (string.charAt(i) == '\n') {
+                if (maxY > maxHeight) {
+                    currentWidth += this.dotWidth;
+                    break;                
+                }
+                if (currentWidth > maxX) {
+                    maxX = currentWidth;
+                }
+                maxY += this.fontHeight + lineSpace;
+                currentWidth = 0;
+            }
+        }
+        
+        if (currentWidth > maxX) {
+            maxX = currentWidth;
+        }
         
         return new Dimension(maxX, maxY);
     }
