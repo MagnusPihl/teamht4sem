a52d55472058d7d0b3ec1c71b83454e1c4e8e2fa
lmklmx <lmklmx@0d024abc-6e28-0410-acd8-a90324f98aa6>
2007-05-16 09:55:50 +0000

    field exploration, treesets out

--- TVP4/src/game/entitycontrol/BreadthFirstAlgorithm.java
+++ TVP4/src/game/entitycontrol/BreadthFirstAlgorithm.java
@@ -28,7 +28,7 @@ import field.*;
  */
 public class BreadthFirstAlgorithm implements SearchAlgorithm {            
     private LinkedList open;
-    private Set closed;
+    private LinkedList closed;
     private Map parents;
 
     private Node currentNode;
@@ -38,9 +38,9 @@ public class BreadthFirstAlgorithm implements SearchAlgorithm {
     /** Creates a new instance of Astar */
     public BreadthFirstAlgorithm() {
         this.open = new LinkedList();
-        this.closed = new TreeSet();
+        this.closed = new LinkedList();
         this.parents = new HashMap();
-    }
+    } 
     
     public int search(Node from, Node to) {
         this.open.clear();
@@ -50,28 +50,31 @@ public class BreadthFirstAlgorithm implements SearchAlgorithm {
         this.currentNode = null;
         this.tempNode = null;
         this.connectedNodes = null;
+        System.out.println("f (" + from.getPosition().x + "," + from.getPosition().y + ")");
         
         this.open.add(to);
         while (!this.open.isEmpty()) {                        
             this.currentNode = (Node)this.open.removeFirst();
+            this.closed.add(this.currentNode);
             
             if (this.currentNode.equals(from)) {
                 return from.connectedAt((Node)this.parents.get(from));
             } else {
-                this.closed.add(currentNode);
                 this.connectedNodes = this.currentNode.getConnectedNodes();
                 
+                System.out.println("c (" + currentNode.getPosition().x + "," + currentNode.getPosition().y + ")");
                 for (int i = 0; i < Node.DIRECTION_COUNT; i++) {
                     this.tempNode = this.connectedNodes[i];
                     if (this.tempNode != null) {                    
                         if ((!this.open.contains(this.tempNode))&&(!this.closed.contains(this.tempNode))) {
+                            System.out.println("s " + i + " - (" + this.tempNode.getPosition().x + "," + this.tempNode.getPosition().y + ")");                            
                             if ((this.tempNode.holdsEntity())&&(!this.tempNode.equals(from))) {
                                 this.closed.add(this.tempNode);
                             } else {
-                                this.open.add(this.tempNode);
+                                this.open.add(this.tempNode);                                
                                 this.parents.put(this.tempNode, this.currentNode);                                
                             }
-                        }                      
+                        }
                     }
                 }
             }
@@ -85,8 +88,11 @@ public class BreadthFirstAlgorithm implements SearchAlgorithm {
      * Get an array of directions to move, to get from A to B.
      * Moves are in ascending order.
      */
-    public int[] fullSearch(Node from, Node to) {
-        int result = this.search(from,to);
+    public Map fullSearch(Node from, Node to) {                
+        if (this.search(from,to) != Node.INVALID_DIRECTION) {                        
+            return this.parents;
+        }
+        /*int result = this.search(from,to);
         
         if (result != Node.INVALID_DIRECTION) {            
             int[] moves = new int[this.parents.size()];
@@ -97,10 +103,11 @@ public class BreadthFirstAlgorithm implements SearchAlgorithm {
                 if (next != null) {
                     moves[i] = from.connectedAt((Node)this.parents.get(from));
                 }
+                System.out.println("Search" + moves[i]);
                 from = next;
             }
             return moves;
-        }
+        }*/
      
         return null;        
     }
